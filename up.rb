558377c93429027c8ec9a022509f3a910a3f8fcc
Jeremy Ottley <Gazaunga@gmail.com>
2018-11-05 19:17:43 -0500

    Update up.rb

--- up.rb
+++ up.rb
@@ -2,5 +2,19 @@
 
 packages = %w[
   i3
+  zsh
+  mpv
+  compton
+  redshift
+  i3status
+  dunst
+  youtube-dl
+  qutebrowser
+  cava
+  spotify
+  i3blocks
+  rofi-git
   
 ]
+
+# investigate streamlink
